089733017571c8e923e485839a9c0d0b59232e21
Junren1996 <47577320+Junren1996@users.noreply.github.com>
2019-02-19 06:30:01 -0600

    Update V20180918123527__create_user_table.sql

--- src/main/resources/db/migration/V20180918123527__create_user_table.sql
+++ src/main/resources/db/migration/V20180918123527__create_user_table.sql
@@ -3,3 +3,10 @@ CREATE TABLE public.person
   id serial PRIMARY KEY,
   name varchar(24)
 );
+
+create table PERSONS (
+    ID int not null,
+    first_name varchar(24) not null,
+    last_name varchar(24) not null,
+    department varchar(24) not null
+);
